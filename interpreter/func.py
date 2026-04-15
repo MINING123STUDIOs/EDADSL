a4c0b7538ce123fcf3c32631d3d607cf66a91569
MINING123STUDIOs <124891784+MINING123STUDIOs@users.noreply.github.com>
2026-04-15 15:31:33 +0200

    Refactor str_to_scalar function and clean up code
    Refactor str_to_scalar function to improve regex checks and remove commented-out code.

--- interpreter/func.py
+++ interpreter/func.py
@@ -3,15 +3,9 @@ import re
 def readfile(name):
     with open(name) as f: tmp = f.read()
     return tmp
-
-
+    
 def str_to_scalar(string):
     s = string
-    #re.search( r"^$", s )   [0-9]+(|\.)[0-9]* (([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*)|[0-9]+)
-    #rule = 1
-    #sw = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "." ]
-    #skip = True if s == "" else not s[0] in sw or ( s in ".-+" and len(s) == 1 )
-    #if skip : return None
 
     suffix =     [   "q",   "r",   "y",   "z",   "a",   "f",   "p",  "n",  "u",  "m",  "c",  "d", "k", "M", "G",  "T",  "P",  "E",  "Z",  "Y",  "R", "Q"  ]
     suffix_val = [ "-30", "-27", "-24", "-21", "-18", "-15", "-12", "-9", "-6", "-3", "-2", "-1", "3", "6", "9", "12", "15", "18", "21", "24", "27", "30" ]
@@ -20,7 +14,7 @@ def str_to_scalar(string):
         si = suffix.index(s[len(s)-1])
         s = s[:len(s)-1] + "e" + suffix_val[si]
         r = float(s)
-    elif re.search( r"^(|-|\+)(([0-9]+)|)\.[0-9]*$", s ) or re.search( r"^(|-|\+)(([0-9]+)|)\.[0-9]*e(|-|\+)[0-9]+$", s ):
+    elif re.search( r"^(|-|\+)(([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*))$", s ) or re.search( r"^(|-|\+)(([0-9]*\.[0-9]+)|([0-9]+\.[0-9]*)|[0-9]+)e(|-|\+)[0-9]+$", s ):
         r = float(s)
     elif re.search( r"^(|-|\+)[0-9]+$", s ):
         r = int(s)
